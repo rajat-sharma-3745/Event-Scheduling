4b9014b3e58cb29662b53b754aaf137c5c6a6e5f
Rajat <sharmarajat3745@gmail.com>
2025-11-10 11:09:53 +0530

    created prisma client

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors';
+import 'dotenv/config';
+import userRoutes from './routes/userRoutes.js'
+import eventRoutes from './routes/eventRoutes.js'
+import { errorMiddleware } from './middlewares/error.js';
+import cookieParser from 'cookie-parser';
+
+
+
+
+const app = express();
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Events');
+})
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/events', eventRoutes);
+
+
+
+
+app.use(errorMiddleware)
+
+app.listen(PORT, () => console.log('Server Running on port', PORT))
