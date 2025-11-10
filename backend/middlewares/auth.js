c0feef6e5db3539e038772dbe40d8da6bbdcd0db
Rajat <sharmarajat3745@gmail.com>
2025-11-10 11:22:26 +0530

    created auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,12 +6,20 @@ import { prisma } from "../prismaClient.js";
 export const auth = async (req, res, next) => {
     try {
         const token = req.cookies['token'];
-       
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if (!decoded?.id) {
             return next(new ApiError('Invalid token payload', 401))
         }
-        const user = await prisma.r
+        const user = await prisma.user.findUnique({
+            where: { id: decoded?.id },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                createdAt: true
+            },
+        })
         if (!user) {
             return res.status(401).json({ message: 'User not found' });
         }
